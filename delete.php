e34a0ede60ea0115c7d70764c11bbed1c4cdb0ff
Sirima <164351537+BigS-sama@users.noreply.github.com>
2025-03-09 21:19:45 +0000

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,1 @@
-<?php
-include 'connection.php';
 
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']); 
-    $query = "DELETE FROM users WHERE id = " . $id;
-
-    if (mysqli_query($link, $query)) {
-        echo "Utilisateur supprimé avec succès.";
-    } else {
-        echo "Erreur lors de la suppression : " . mysqli_error($link);
-    }
-
-    mysqli_close($link); 
-    header("Location: read.php");
-    exit();
-}
-?>
