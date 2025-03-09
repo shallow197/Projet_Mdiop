ab41cba1988887e9989721f6805ea4213f56a8a0
shallow197 <locheikhsaliou@gmail.com>
2025-03-09 17:16:06 +0000

    Update update_utilisateurs.php

--- update_utilisateurs.php
+++ update_utilisateurs.php
@@ -7,43 +7,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $prenom = $_POST["prenom"];
     $login = $_POST["login"];
 
-  
     if ($_FILES["pfp"]["name"] != "") {
         $chemin = "pics/"; 
         $new_file = $chemin . basename($_FILES["pfp"]["name"]);
-        $extension = strtolower(pathinfo($new_file, PATHINFO_EXTENSION));
+        $extension_image = strtolower(pathinfo($new_file, PATHINFO_EXTENSION));
 
-      
         $extensions = ["jpg", "jpeg", "png", "gif"];
-        if (!in_array($extension, $extensions)) {
-            die("Seuls les fichiers JPG, JPEG, PNG et GIF sont autorisés.");
+        if (!in_array($extension_image, $extensions)) 
+        {
+            die("La photo de profil ne peut être qu'une image !");
+        }
+
+        if (!move_uploaded_file($_FILES["pfp"]["tmp_name"], $new_file)) 
+        {
+            die("Une erreur a été rencontrée lors du téléchargement de l'image.");
         }
 
         
-        if (!move_uploaded_file($_FILES["pfp"]["tmp_name"], $new_file)) {
-            die("Erreur lors de l'upload de l'image.");
-        }
-
-       
-        $query = $link->prepare("UPDATE users SET nom=?, prenom=?, login=?, pfp=? WHERE id=?");
-        $query->bind_param("ssssi", $nom, $prenom, $login, $new_file, $id);
+        $query = mysqli_prepare($link, "UPDATE users SET nom=?, prenom=?, login=?, pfp=? WHERE id=?");
+        mysqli_stmt_bind_param($query, "ssssi", $nom, $prenom, $login, $new_file, $id);
     } 
     else 
     {
-        $query = $link->prepare("UPDATE users SET nom=?, prenom=?, login=?, pfp=? WHERE id=?");
-        $query->bind_param("ssssi", $nom, $prenom, $login, $new_file, $id);
+        
+        $query = mysqli_prepare($link, "UPDATE users SET nom=?, prenom=?, login=? WHERE id=?");
+        mysqli_stmt_bind_param($query, "sssi", $nom, $prenom, $login, $id);
     }
 
-    if ($query->execute()) 
+    if (mysqli_stmt_execute($query)) 
     {
-        echo "<script>alert('Utilisateur mis à jour avec succès !'); window.location.href='read.php';</script>";
+        echo "<script>
+             alert('Utilisateur mis à jour avec succès !'); 
+             window.location.href='read.php';
+             </script>";
     } 
     else 
     {
-        echo "Erreur lors de la mise à jour : " . $conn->error;
+        echo "Erreur lors de la mise à jour : " . mysqli_error($link);
     }
 
-    $query->close();
-    $conn->close();;
+    mysqli_stmt_close($query);
+    mysqli_close($link);
 }
 ?>
