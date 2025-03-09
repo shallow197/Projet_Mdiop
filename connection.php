7438e3dd404beb9c0af61f33a0bbc248384f8713
shallow197 <locheikhsaliou@gmail.com>
2025-03-09 11:27:09 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,8 @@ $password = "";
 $bdd = "essai1";
 
 
-$link =  mysqli_connect($host, $user, $password, $bdd) or die("erreur de connexion au serveur");;
+$link =  mysqli_connect($host, $user, $password) or die("erreur de connexion au serveur");
+$bd_select = mysqli_select_db($link, $bdd) or die("erreur de connexion a la base de donnees");
 
 
 ?>
