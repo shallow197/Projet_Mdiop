584f2f53e35814ab15674e4c55649ef6e903eb8d
Sirima <164351537+BigS-sama@users.noreply.github.com>
2025-03-10 16:35:52 +0000

    Update create.php

--- create.php
+++ create.php
@@ -1,1 +1,39 @@
+<?php 
+	include 'connection.php';
+
+
+    $prenom = $_POST["prenom"];
+    $name = $_POST["name"];
+    $password = password_hash($_POST['mdp'], PASSWORD_BCRYPT);
+    $login = $_POST["login"];
+    $profile_pic = "";
+    if (isset($_FILES["pfp"])) 
+    {
+	        $file = basename($_FILES['pfp']['name']);
+            $tmp = $_FILES['pfp']['tmp_name'];
+            $new_file = "pics/" . $file;
+            move_uploaded_file($tmp, $new_file);
+            $profile_pic = $new_file;
+            
+    }
+
+    $query = "INSERT INTO users(nom, prenom, login, password, pfp)  VALUES ('$name', '$prenom', '$login', '$password', '$profile_pic')";
+    $result = mysqli_query($link, $query);
+  
+    if ($result) 
+    {
+        echo "<script>
+	      alert('Nouvel utilisateur enregistré avec succès !'); 
+          window.location.href='create.html';
+ 	      </script>";
+    } 
+    else 
+    {
+        echo "Une erreur a été rencontrée lors de l'ajout !";
+    }
+
+  
+    mysqli_close($link);
+
+?>
 
