7fb751fe57ef1f75054cdf9fd7a3607b24ce694d
shallow197 <locheikhsaliou@gmail.com>
2025-03-10 11:43:22 +0000

    Update create.php

--- create.php
+++ create.php
@@ -1,19 +1,20 @@
 <?php 
 	include 'connection.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $prenom = $_POST["prenom"];
     $name = $_POST["name"];
     $password = password_hash($_POST['mdp'], PASSWORD_BCRYPT);
     $login = $_POST["login"];
     $profile_pic = "";
-    if (isset($_FILES["pfp"])) {
-	    $file = basename($_FILES['pfp']['name']);
+    if (isset($_FILES["pfp"])) 
+    {
+	        $file = basename($_FILES['pfp']['name']);
             $tmp = $_FILES['pfp']['tmp_name'];
             $new_file = "pics/" . $file;
             move_uploaded_file($tmp, $new_file);
             $profile_pic = $new_file;
+            
     }
 
     $query = "INSERT INTO users(nom, prenom, login, password, pfp)  VALUES ('$name', '$prenom', '$login', '$password', '$profile_pic')";
@@ -22,16 +23,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result) 
     {
         echo "<script>
-	      alert('Nouvel utilisateur enregistré avec succès!'); 
+	      alert('Nouvel utilisateur enregistré avec succès !'); 
           window.location.href='create.html';
  	      </script>";
     } 
     else 
     {
-        echo "Erreur lors de l'ajout : " . mysqli_error($link);
+        echo "Une erreur a été rencontrée lors de l'ajout !";
     }
 
   
     mysqli_close($link);
-}
+
 ?>
