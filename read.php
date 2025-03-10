efd4c34c5f5c0757e907cc307a647af6d5dd22ae
shallow197 <locheikhsaliou@gmail.com>
2025-03-10 12:16:45 +0000

    Update read.php

--- read.php
+++ read.php
@@ -20,13 +20,14 @@ include 'connection.php';
     <a href="create.html" class="nav-button">Ajout d'utilisateurs</a>
 </nav>
 
-</nav>
-
 <div class="container">
     <div class="content">
         <h2>Utilisateurs</h2>
-        <table>
+
+        <form action="delete_multiple.php" method="post">
+            <table>
                 <tr>
+                    <th><input type="checkbox" id="selectAll" onclick="toggle(this)"></th> 
                     <th>ID</th>
                     <th>Nom</th>
                     <th>Prenom</th>
@@ -34,41 +35,51 @@ include 'connection.php';
                     <th>Photo de profil</th>
                     <th>Action</th>
                 </tr>
-            <tbody>
-            <?php
-                $query = "SELECT id, nom, prenom, login, pfp FROM users ORDER BY id";
-                $result = mysqli_query($link, $query);
+                <tbody>
+                <?php
+                    $query = "SELECT id, nom, prenom, login, pfp FROM users ORDER BY id";
+                    $result = mysqli_query($link, $query);
 
-                if ($result->num_rows > 0) 
-                {
-                   while ($row = mysqli_fetch_assoc($result)) 
-                   {
-                    $profile_pic = htmlspecialchars($row["pfp"]);
-                        
-                        echo "<tr>
-                                <td>" . htmlspecialchars($row["id"]) . "</td>
-                                <td>" . htmlspecialchars($row["nom"]) . "</td>
-                                <td>" . htmlspecialchars($row["prenom"]) . "</td>
-                                <td>" . htmlspecialchars($row["login"]) . "</td>
-                                <td><img src='". $profile_pic."'width='50' height='50' style='border-radius: 50%;'></td>
-                                <td>
-                                    <a href='update.php?id=" . htmlspecialchars($row["id"]) . "'>modifier</a><br> 
-                                    <a href='delete.php?id=" . htmlspecialchars($row["id"]) . "' onclick='return confirm(\"Êtes-vous sûr(e) de vouloir supprimer cet utilisateur ?\");'>supprimer</a>
-                                </td>
-                              </tr>";
+                    if ($result->num_rows > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $profile_pic = htmlspecialchars($row["pfp"]);
+
+                            echo "<tr>
+                                    <td><input type='checkbox' name='users_to_delete[]' value='" . htmlspecialchars($row["id"]) . "'></td>
+                                    <td>" . htmlspecialchars($row["id"]) . "</td>
+                                    <td>" . htmlspecialchars($row["nom"]) . "</td>
+                                    <td>" . htmlspecialchars($row["prenom"]) . "</td>
+                                    <td>" . htmlspecialchars($row["login"]) . "</td>
+                                    <td><img src='". $profile_pic."' width='50' height='50' style='border-radius: 50%;'></td>
+                                    <td>
+                                        <a href='update.php?id=" . htmlspecialchars($row["id"]) . "'>modifier</a><br> 
+                                        <a href='delete.php?id=" . htmlspecialchars($row["id"]) . "' onclick='return confirm(\"Êtes-vous sûr(e) de vouloir supprimer cet utilisateur ?\");'>supprimer</a>
+                                    </td>
+                                  </tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='7'>Aucun utilisateur inscrit.</td></tr>";
                     }
-                } 
-                else 
-                {
-                    echo "<tr><td colspan='6'>Aucun utilisateur inscrit.</td></tr>";
-                }
 
-                mysqli_close($link);
-            ?>
-            </tbody>
-        </table>
+                    mysqli_close($link);
+                ?>
+                </tbody>
+            </table>
+
+            <button type="submit" onclick="return confirm('Êtes-vous sûr(e) de vouloir supprimer ces utilisateurs ?');">Supprimer les utilisateurs sélectionnés</button>
+        </form>
+
     </div>
 </div>
+
+<script>
+function toggle(source) {
+    checkboxes = document.getElementsByName('users_to_delete[]');
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
+</script>
 
 </body>
 </html>
