101e8b3bf423b0da3ab14fdbc8b206b2ae2ac95d
shallow197 <locheikhsaliou@gmail.com>
2025-03-10 11:47:33 +0000

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,24 @@
 <?php
 include 'connection.php';
 
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']); 
-    $query = "DELETE FROM users WHERE id = " . $id;
+    $id = $_GET['id']; 
 
-    if (mysqli_query($link, $query)) {
-        echo "Utilisateur supprimé avec succès.";
-    } else {
-        echo "Erreur lors de la suppression : " . mysqli_error($link);
+    $query = "DELETE FROM users WHERE id = " . $id;
+    $result = mysqli_query($link, $query);
+
+    if ($result) 
+    {
+        echo "<script>
+        alert('Utilisateur supprimé avec succès !'); 
+        window.location.href='read.php';
+        </script>";
+    } 
+    else 
+    {
+        echo "Une erreur a été rencontrée lors de la suppression !";
     }
 
     mysqli_close($link); 
-    header("Location: read.php");
     exit();
-}
-?>
 
+?>
