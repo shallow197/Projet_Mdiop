0401a4afbde40b95da75c07235e6353aa4411c75
shallow197 <locheikhsaliou@gmail.com>
2025-03-15 20:23:12 +0000

    Update administrateur.php

--- administrateur.php
+++ administrateur.php
@@ -1,35 +1,39 @@
 <?php
 session_start();
-if (isset($_POST['login'])) 
-{
-    $_SESSION['login'] = $_POST['login'];
-}
+
 include 'connection.php';
 
-$login = $_POST['login'];
-$mdp_hashe = hash('sha256', $_POST['password']);
+if (isset($_POST['login'])) 
+{
+    $login = $_POST['login'];
+    $mdp_hashe = hash('sha256', $_POST['password']);
 
-$query = "SELECT * FROM admins WHERE login = '$login' AND password = '$mdp_hashe'";
-$result = mysqli_query($link, $query);
+    $query = "SELECT * FROM admins WHERE login = '$login' AND password = '$mdp_hashe'";
+    $result = mysqli_query($link, $query);
 
     if (mysqli_num_rows($result) > 0) 
     {
-    $admin = mysqli_fetch_assoc($result);
-    $prenom_admin = htmlspecialchars($admin['prenom']);
-    $nom_admin = htmlspecialchars($admin['nom']);
-    echo "<script>
-             alert('Bienvenue administrateur $prenom_admin $nom_admin!');
-             window.location.href='create.html';
-          </script>";
-    }
+        $admin = mysqli_fetch_assoc($result);
+        $_SESSION['login'] = $admin['login']; 
+        $_SESSION['prenom'] = $admin['prenom']; 
+        $_SESSION['nom'] = $admin['nom']; 
+
+        $prenom_admin = htmlspecialchars($admin['prenom']);
+        $nom_admin = htmlspecialchars($admin['nom']);
+
+        echo "<script>
+                 alert('Bienvenue administrateur $prenom_admin $nom_admin!');
+                 window.location.href='create.php';
+              </script>";
+    } 
     else 
     {
-    echo "<script>
-             alert('Login ou mot de passe incorrect !'); 
-             window.location.href='admin.php';
-             </script>";
+        echo "<script>
+                 alert('Login ou mot de passe incorrect !'); 
+                 window.location.href='admin.php';
+              </script>";
     } 
 
-mysqli_close($link);
+    mysqli_close($link);
+}
 ?>
-
