503957ac86ee5f4b16fbcefeaee267573e61bd2f
Sirima <164351537+BigS-sama@users.noreply.github.com>
2025-03-15 16:43:01 +0000

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,25 @@
+<?php
+include 'connection.php';
+
+    $id = $_GET['id']; 
+
+    $query = "DELETE FROM users WHERE id = " . $id;
+    $result = mysqli_query($link, $query);
+
+    if ($result) 
+    {
+        echo "<script>
+        alert('Utilisateur supprimé avec succès !'); 
+        window.location.href='read.php';
+        </script>";
+    } 
+    else 
+    {
+        echo "Une erreur a été rencontrée lors de la suppression !";
+    }
+
+    mysqli_close($link); 
+    exit();
+
+?>
 
