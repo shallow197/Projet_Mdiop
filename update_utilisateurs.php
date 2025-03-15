3a878983fe10a3a2fcda4e91fbcd1fc143c53261
Sirima <164351537+BigS-sama@users.noreply.github.com>
2025-03-15 18:54:31 +0000

    Add files via upload

--- update_utilisateurs.php
+++ update_utilisateurs.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: admin.php"); 
+    exit();
+}
 if (isset($_POST['login'])) 
 {
     $_SESSION['login'] = $_POST['login'];
