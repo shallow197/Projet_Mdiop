7e42c0647b5f91f3d9611fa6459aec94cf0e406a
shallow197 <locheikhsaliou@gmail.com>
2025-03-16 20:09:59 +0000

    Update update.php

--- update.php
+++ update.php
@@ -8,19 +8,7 @@ if (!isset($_SESSION['login']))
 
 include 'connection.php';
 
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 20)) 
-{
-    session_unset();
-    session_destroy();
-    echo "<script>
-            alert('Vous êtes déconnecté(e) pour inactivité.');
-            window.location.href='admin.php';
-          </script>";
-    exit();
-}
 
-
-$_SESSION['last_activity'] = time();
 
 $id = $_GET['id']; 
 $query = "SELECT * FROM users WHERE id = " . $id;
@@ -44,7 +32,7 @@ mysqli_close($link);
         {
             clearTimeout(timeout);
             timeout = setTimeout(() => {
-                alert("Session expirée pour inactivité.");
+                alert("Vous êtes déconnecté(e) pour inactivité.");
                 window.location.href = "admin.php";
             }, 20000); 
         }
@@ -100,4 +88,3 @@ mysqli_close($link);
 
 </body>
 </html>
-
